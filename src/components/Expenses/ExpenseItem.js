eced9dba216b825bbed3a857ebb00827816d0543
moizajmal <mzajmal08@gmail.com>
2023-08-17 15:56:50 +0530

    Added use state for changing value from UI

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,18 @@
 import './ExpenseItem.css'
 import ExpenseDate  from './ExpenseDate';
 import Card from '../UI/Card';
+import React,{useState} from 'react';
 function ExpenseItem(props)
 {
+    const[title, updatedTitle] = useState(props.title);
+    const clickHandler = ()=>{updatedTitle('Updated!');console.log(title)};
   
-    return (
+  return (
         <Card className='expense-item'>
            <ExpenseDate date={props.date}></ExpenseDate>
-            <div className='expense-item__description'><h2>{props.title}</h2></div>
+            <div className='expense-item__description'><h2>{title}</h2></div>
             <div className='expense-item__price'>{props.amount}</div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card> 
 
     );
